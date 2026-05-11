849235a54291169cd0beb99c1879dd9cf616abdc
KaguraKaerei <kaerei86@gmail.com>
2026-05-12 00:07:57 +0800

    feat(app): 添加入口文件

---
+++ src/app/entry.h
@@ -0,0 +1,48 @@
+#ifndef _entry_h_
+#define _entry_h_
+
+// ! app ! //
+
+
+
+// ! service ! //
+
+
+
+// ! device ! //
+
+
+
+// ! domain ! //
+
+
+
+// ! infra ! //
+
+
+
+// ! platform ! //
+
+
+
+// ! ========================= 接 口 变 量 / Typedef 声 明 ========================= ! //
+
+
+
+// ! ========================= 接 口 函 数 声 明 ========================= ! //
+
+/**
+ * @brief 程序初始化入口函数
+ */
+static inline void entry_init(void) {
+
+}
+
+/**
+ * @brief 程序主循环入口函数
+ */
+static inline void entry_loop(void) {
+
+}
+
+#endif
